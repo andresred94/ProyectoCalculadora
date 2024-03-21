73bd840aea4f2aa977ed2be05a67adeb237fc81b
Andoru <113431513+andresred94@users.noreply.github.com>
2024-03-21 15:08:36 +0100

    Instantanea 5

--- src/calculadora/Resta.java
+++ src/calculadora/Resta.java
@@ -2,7 +2,8 @@ package calculadora;
 // Instantanea 1 
 // Instantanea 2
 // Instantanea 3
-// Instantanea 4
+// Instantanea 4 
+// Instantanea 5 
 
 public class Resta extends Operacion {
 	public void operar() {
